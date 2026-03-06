2ff49106589cfdf3c26cbf6d131723ecc6417a2d
Randson Nunes <randson_1@yahoo.com.br>
2026-03-06 09:49:58 -0300

    Add ESLint configuration for TypeScript and Prettier
    Add ESLint configuration for TypeScript and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+        semi: true,
+      },
+    ],
+  },
+};
